7240c93434c9b7eefbb6d7a6972fb850d3f3fa5f
Master <39118305+mono1981163@users.noreply.github.com>
2022-03-17 15:38:46 +0100

    2022/03/17

--- src/components/subpages/imprint.tsx
+++ src/components/subpages/imprint.tsx
@@ -14,6 +14,9 @@ export default function Imprint() {
     <Grid>
       <Navigation type={'submenu'} />
       <Grid className={classes.mainGrid}>
+        <Typography className={classes.largeTitle}>
+            {t("IMPRINTMAINTITLE")}
+        </Typography>
         <Typography className={classes.title}>
             {t("IMPRINTTITLE")}
         </Typography>
--- src/components/subpages/styles/imprintstyle.tsx
+++ src/components/subpages/styles/imprintstyle.tsx
@@ -7,6 +7,11 @@ export const useStyles = makeStyles((theme: Theme) => createStyles({
         width: '900px',
         wordBreak: "break-word"
     },
+    largeTitle: {
+        fontSize: '50px',
+        color: '#020202',
+        margin: "0 0 15px"
+    },
     title: {
         fontSize: '18px',
         fontWeight: 'bold',
@@ -14,7 +19,7 @@ export const useStyles = makeStyles((theme: Theme) => createStyles({
         lineHeight: '2'
     },
     contents: {
-        margin: '30px 0',
+        margin: '15px 0',
         fontSize: '18px',
         color: '#4A4848',
         letterSpacing: '1px',
@@ -33,6 +38,9 @@ export const useStyles = makeStyles((theme: Theme) => createStyles({
         mainGrid: {
             margin: '30px 15px 130px',
             width: 'auto'
-        }
+        },
+        largeTitle: {
+            fontSize: '40px'
+        },
     }
 }))
