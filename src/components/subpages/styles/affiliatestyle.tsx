7f43991458fd25aa0ef7e051ad749909abd6f44d
Yawata <91160380+goldsun27@users.noreply.github.com>
2022-05-18 18:34:07 +0200

    update privacy page

--- src/components/subpages/styles/affiliatestyle.tsx
+++ src/components/subpages/styles/affiliatestyle.tsx
@@ -1,83 +1,86 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
-export const useStyles = makeStyles((theme: Theme) => createStyles({
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     mainGrid: {
-        whiteSpace: 'pre-line',
-        margin: '100px auto 200px',
-        width: '1000px',
-        wordBreak: "break-word"
+      whiteSpace: 'pre-line',
+      margin: '100px auto 200px',
+      maxWidth: '1000px',
+      padding: '0px 20px',
+      wordBreak: 'break-word',
     },
     largeTitle: {
-        fontSize: '50px',
-        color: '#020202',
-        margin: "0 0 15px",
-        textAlign: 'center'
+      fontSize: '50px',
+      color: '#020202',
+      margin: '0 0 15px',
+      textAlign: 'center',
     },
     title: {
-        fontSize: '30px',
-        fontWeight: 'bold',
-        color: '#020202',
-        textAlign: 'center'
+      fontSize: '30px',
+      fontWeight: 'bold',
+      color: '#020202',
+      textAlign: 'center',
     },
     subtitle: {
-        fontSize: '22px',
-        fontWeight: 'bold',
-        color: '#020202',
-        marginTop: '30px'
+      fontSize: '22px',
+      fontWeight: 'bold',
+      color: '#020202',
+      marginTop: '30px',
     },
     subtitleCenter: {
-        fontSize: '22px',
-        fontWeight: 'bold',
-        color: '#020202',
-        marginTop: '30px',
-        textAlign: 'center'
+      fontSize: '22px',
+      fontWeight: 'bold',
+      color: '#020202',
+      marginTop: '30px',
+      textAlign: 'center',
     },
     contents: {
-        fontSize: '16px',
-        margin: "50px 0",
-        color: '#4A4848',
-        letterSpacing: '1px',
-        lineHeight: '2'
+      fontSize: '16px',
+      margin: '50px 0',
+      color: '#4A4848',
+      letterSpacing: '1px',
+      lineHeight: '2',
     },
     subitem: {
-        display: 'flex',
-        margin: '30px 0'
+      display: 'flex',
+      margin: '30px 0',
     },
     subChildItem: {
-        display: 'flex',
-        margin: '10px 0'
+      display: 'flex',
+      margin: '10px 0',
     },
     subItemNumber: {
-        fontSize: '16px',
-        width: '30px',
-        flexShrink: 0,
-        color: '#4A4848'
+      fontSize: '16px',
+      width: '30px',
+      flexShrink: 0,
+      color: '#4A4848',
     },
     subItemContent: {
-        fontSize: '16px',
-        letterSpacing: '1px',
-        color: '#4A4848'
+      fontSize: '16px',
+      letterSpacing: '1px',
+      color: '#4A4848',
     },
     [theme.breakpoints.down(600)]: {
-        mainGrid: {
-            margin: '30px 15px 130px',
-            width: 'auto'
-        },
-        largeTitle: {
-            fontSize: '40px'
-        },
-        title: {
-            fontSize: '25px',
-        },
-        subtitle: {
-            fontSize: '20px'
-        },
-        subtitleCenter: {
-            fontSize: '20px'
-        },
-        contents: {
-            fontSize: '14px',
-            margin: "30px 0"
-        },
-    }
-}))
+      mainGrid: {
+        margin: '30px 15px 130px',
+        width: 'auto',
+      },
+      largeTitle: {
+        fontSize: '40px',
+      },
+      title: {
+        fontSize: '25px',
+      },
+      subtitle: {
+        fontSize: '20px',
+      },
+      subtitleCenter: {
+        fontSize: '20px',
+      },
+      contents: {
+        fontSize: '14px',
+        margin: '30px 0',
+      },
+    },
+  }),
+)
--- src/components/subpages/styles/termsstyles.tsx
+++ src/components/subpages/styles/termsstyles.tsx
@@ -1,52 +1,55 @@
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
-export const useStyles = makeStyles((theme: Theme) => createStyles({
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
     mainGrid: {
-        whiteSpace: 'pre-line',
-        margin: '100px auto 200px',
-        width: '1000px',
-        wordBreak: "break-word"
+      whiteSpace: 'pre-line',
+      margin: '100px auto 200px',
+      maxWidth: '1000px',
+      padding: '0px 20px',
+      wordBreak: 'break-word',
     },
     largeTitle: {
-        fontSize: '50px',
-        color: '#020202',
-        margin: "0 0 15px"
+      fontSize: '50px',
+      color: '#020202',
+      margin: '0 0 15px',
     },
     title: {
-        fontSize: '30px',
-        fontWeight: 'bold',
-        color: '#020202'
+      fontSize: '30px',
+      fontWeight: 'bold',
+      color: '#020202',
     },
     item: {
-        display: 'flex',
-        margin: '30px 0'
+      display: 'flex',
+      margin: '30px 0',
     },
     itemNumber: {
-        fontSize: '16px',
-        width: '30px',
-        flexShrink: 0,
-        color: '#4A4848',
-        lineHeight: 2
+      fontSize: '16px',
+      width: '30px',
+      flexShrink: 0,
+      color: '#4A4848',
+      lineHeight: 2,
     },
     contents: {
-        fontSize: '16px',
-        color: '#4A4848',
-        letterSpacing: '1px',
-        lineHeight: '2'
+      fontSize: '16px',
+      color: '#4A4848',
+      letterSpacing: '1px',
+      lineHeight: '2',
     },
     [theme.breakpoints.down(600)]: {
-        mainGrid: {
-            margin: '30px 15px 130px',
-            width: 'auto'
-        },
-        largeTitle: {
-            fontSize: '40px'
-        },
-        title: {
-            fontSize: '25px',
-        },
-        contents: {
-            fontSize: '14px'
-        },
-    }
-}))
+      mainGrid: {
+        margin: '30px 15px 130px',
+        width: 'auto',
+      },
+      largeTitle: {
+        fontSize: '40px',
+      },
+      title: {
+        fontSize: '25px',
+      },
+      contents: {
+        fontSize: '14px',
+      },
+    },
+  }),
+)
